4a40d5690c00e07aaa678832c099fc4d7302e66c
Lex-QA <dev4lex@gmail.com>
2025-07-14 21:08:44 +0300

    add users fixture, assertions and client

---
+++ clients/users/users_schema.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+from tools.fakers import fake
+
+
+class RegisterData(BaseModel):
+    id: int
+    login: str
+    password: str = Field(alias="pass")
+    games: list[str]
+
+
+class Info(BaseModel):
+    status: str
+    message: str
+
+
+class UserSchema(BaseModel):
+    """
+    Описание структуры пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    register_data: RegisterData
+    info: Info
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    login: str = Field(default_factory=fake.email)
+    password: str = Field(alias="pass", default_factory=fake.password)
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    register_data: RegisterData
+    info: Info
+
+class GetUserResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    id: int
+    login: str
+    password: str = Field(alias="pass")
+    games: list[str]
---
+++ tools/assertions/users.py
@@ -0,0 +1,56 @@
+import allure
+
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+    UserSchema
+from tools.assertions.base import assert_equal
+from tools.logger import get_logger
+
+logger = get_logger("USERS_ASSERTIONS")
+
+
+@allure.step("Check create user response")
+def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
+    """
+    Проверяет, что ответ на создание пользователя соответствует запросу.
+
+    :param request: Исходный запрос на создание пользователя.
+    :param response: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    logger.info("Check create user response")
+
+    assert_equal(response.register_data.login, request.login, "login")
+    assert_equal(response.register_data.password, request.password, "password")
+
+
+@allure.step("Check user")
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
+
+    :param actual: Фактические данные пользователя.
+    :param expected: Ожидаемые данные пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    logger.info("Check user")
+
+    assert_equal(actual.register_data.login, expected.register_data.login, "login")
+    assert_equal(actual.register_data.password, expected.register_data.password, "pass")
+
+
+@allure.step("Check get user response")
+def assert_get_user_response(
+        get_user_response: GetUserResponseSchema,
+        create_user_response: CreateUserResponseSchema
+):
+    """
+    Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
+
+    :param get_user_response: Ответ API при запросе данных пользователя.
+    :param create_user_response: Ответ API при создании пользователя.
+    :raises AssertionError: Если данные пользователя не совпадают.
+    """
+    logger.info("Check get user response")
+
+    assert get_user_response.login == create_user_response.register_data.login
+    assert get_user_response.password == create_user_response.register_data.password
